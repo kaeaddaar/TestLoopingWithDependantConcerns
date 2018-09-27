a8560de31780d13aed05dfddbbf1a153a7d37aca
kaeaddaar <playinfc@hotmail.ca>
2018-09-26 17:37:35 -0700

    init2

---
+++ SupercedeBehaviorOfInnerLoop/Looping.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace SupercedeBehaviorOfInnerLoop
+{
+    public static class Looping
+    {
+        static readonly int[] OriginalList = new int[] { 1, 2, 3, 4, 5 };
+                
+        public static List<int> funcTransform(int multiplier)
+        {
+            List<int> NewSet = new List<int>(OriginalList);
+            NewSet = NewSet.Select(item => item * multiplier).ToList();
+            return NewSet;
+        }
+
+        public static List<int> funcTransformMaxMuliplierOnly(int[] PossibleMultipliers)
+        {
+            int multiplier = PossibleMultipliers.Max(m => m);
+            List<int> NewSet = new List<int>(OriginalList);
+            NewSet = NewSet.Select(item => item * multiplier).ToList();
+            return NewSet;
+        }
+    }
+}
